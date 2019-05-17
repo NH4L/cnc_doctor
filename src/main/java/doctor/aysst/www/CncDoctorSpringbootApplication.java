5298d7c6744474a548e531c07b1516e218dab9a2
NH4L <nh4l@qq.com>
2019-05-18 03:59:59 +0800

    first commit

---
+++ src/main/java/doctor/aysst/www/CncDoctorSpringbootApplication.java
@@ -0,0 +1,13 @@
+package doctor.aysst.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CncDoctorSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CncDoctorSpringbootApplication.class, args);
+    }
+
+}
